993d8ff1a7c28d1d6a55b473e6df175989681fe1
milandrljaca <milandrljaca@gmail.com>
2021-04-10 22:06:58 +0200

    Implemented Model

---
+++ WebGentle.BookStore/WebGentle.BookStore/Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebGentle.BookStore.Models;
+using WebGentle.BookStore.Repository;
+
+namespace WebGentle.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository= null;
+        
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        
+        public List<BookModel> GetAllBooks()
+        {
+            return _bookRepository.GetAllBooks();
+        }
+
+        public BookModel GetBook(int id)
+        {
+            return _bookRepository.GetbookById(id);
+        }
+        public List<BookModel> SearchBooks(string bookName, string authorName)
+        {
+            return _bookRepository.SearchBook(bookName,authorName);
+        }
+    }
+}
---
+++ WebGentle.BookStore/WebGentle.BookStore/Repository/BookRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebGentle.BookStore.Models;
+
+namespace WebGentle.BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel GetbookById(int id)
+        {
+            return DataSource().Where(x=> x.Id==id).FirstOrDefault();
+        }
+        public List<BookModel> SearchBook(string title, string author)
+        {
+            return DataSource().Where(x=> x.Title.Contains(title) && x.Author.Contains(author)).ToList();
+        }
+
+        private  List<BookModel>DataSource ()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){Id=1 , Title="MVC",Author="glavonja"},
+                new BookModel(){Id=2 , Title="dot net core",Author="nitish"},
+                new BookModel(){Id=3 , Title="C#",Author="monika"},
+                new BookModel(){Id=4 , Title="Java",Author="webgentle"},
+                new BookModel(){Id=5 , Title="PHP",Author="glavonja"},
+
+
+            };
+        }
+    }
+}
